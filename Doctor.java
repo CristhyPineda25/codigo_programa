4aaa56c0c324f57758572f30975458bf5240cf60
YajairaEnamorado <enamoradotorres27@gmail.com>
2024-07-17 20:39:36 -0600

    Add files via upload

---
+++ Doctor.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.doctor;
+
+/**
+ *
+ * @ Michel Enamorado
+ */
+public class Doctor {
+    
+    package subclasepersona;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad);
+    }
+}
+
+public class Doctor extends Persona {
+    private String especialidad;
+
+    public Doctor(String nombre, int edad, String especialidad) {
+        super(nombre, edad);
+        this.especialidad = especialidad;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Especialidad: " + especialidad);
+    }
+
+    public void diagnosticar() {
+        System.out.println("Dr. " + getNombre() + " está diagnosticando...");
+    }
+}
